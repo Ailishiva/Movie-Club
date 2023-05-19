65f2dc1042c39dfff71afd0a9d385c9c1324f3dc
Aili ShivaPrasad <112164679+Ailishiva@users.noreply.github.com>
2023-05-19 09:00:21 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-
-const elem =   <BrowserRouter><App /></BrowserRouter>
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(elem);
